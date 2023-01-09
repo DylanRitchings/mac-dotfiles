22f2798a838ec5857fb11c3a975139c9f95246d3
dritchings <dritchings@qa.com>
2023-01-09 14:37:47 +0000

    added direct monitor automation files

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,18 +3,8 @@ local application = require("hs.application")
 
 local menu = hs.menubar.new()
 
--- local function setDirectMon(path, status)
---   local file = io.open(path, "r")
---   local data = file:read("*a")
---   file:close()
---   menu:setTitle(status)
---   menu:setTooltip("DM Toggle")
---   hs.osascript.applescript([[display dialog "]] .. path .. [["]])
---   hs.osascript.applescript(data)
--- end
-
 local function setDirectOn()
-  local file = io.open("/Users/dylan/Dev/Scripts/Mac automation/direct_monitor/direct_on.applescript", "r")
+  local file = io.open("/Users/dylan/.hammerspoon/direct_monitor/direct_on.applescript", "r")
   local data = file:read("*a")
   file:close()
   menu:setTitle("DM is on")
@@ -23,7 +13,7 @@ local function setDirectOn()
 end
 
 local function setDirectOff()
-  local file = io.open("/Users/dylan/Dev/Scripts/Mac automation/direct_monitor/direct_off.applescript", "r")
+  local file = io.open("/Users/dylan/.hammerspoon/direct_monitor/direct_off.applescript", "r")
   local data = file:read("*a")
   file:close()
   menu:setTitle("DM is Off")
@@ -32,10 +22,10 @@ local function setDirectOff()
 end
 
 local function enableMenu()
-  local onFile = "/Users/dylan/Dev/Scripts/Mac automation/direct_monitor/direct_on.applescript"
-  local offFile = "/Users/dylan/Dev/Scripts/Mac automation/direct_monitor/direct_off.applescript"
+  local onFile = "/Users/dylan/.hammerspoon/direct_monitor/direct_on.applescript"
+  local offFile = "/Users/dylan/.hammerspoon/direct_monitor/direct_off.applescript"
 
-  menu:setTitle("|||")
+  menu:setTitle("||")
   menu:setTooltip("DM")
   menu:setMenu({
     { title = "Direct Monitor On", fn = setDirectOn, },
